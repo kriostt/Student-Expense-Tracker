f2cca36345a551c6f5ef9f1120c2d6cf8024aacf
kriostt <129346100+kriostt@users.noreply.github.com>
2024-04-07 20:00:30 -0600

    Added [Authorize] to BudgetController

--- StudentExpenseTracker/StudentExpenseTracker/Controllers/BudgetController.cs
+++ StudentExpenseTracker/StudentExpenseTracker/Controllers/BudgetController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StudentExpenseTracker.Models;
 using System.Linq;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace StudentExpenseTracker.Controllers
 {
+    [Authorize]
     public class BudgetController : Controller
     {
         private readonly TransactionContext _context;
